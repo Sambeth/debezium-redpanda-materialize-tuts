49996c6a94135f30e01c5a1de8830287b7edb769
sambeth <sambethslim@gmail.com>
2022-03-16 23:48:28 +0000

    change debezium postgres db to postgresdb 12 which has pgoutput for logical decoding without needing to install plugins

--- scripts/register_connector.py
+++ scripts/register_connector.py
@@ -17,16 +17,19 @@ def register_connector(url, connector_config):
 
 if __name__ == "__main__":
     connector_config = {
-      "name": "debezium-postgres-connector",
+      "name": "debezium-tracking-connector",
       "config": {
         "connector.class": "io.debezium.connector.postgresql.PostgresConnector",
+        "plugin.name": "pgoutput",
         "database.hostname": "postgres",
         "database.port": "5432",
         "database.user": "postgres",
         "database.password": "postgres",
         "database.dbname": "postgres",
         "database.server.name": "debezium",
-        "schema.whitelist": "inventory"
+        "schema.whitelist": "tracking",
+        "publication.name": "mytestpub",
+        "publication.autocreate.mode": "filtered"
       }
     }
 
