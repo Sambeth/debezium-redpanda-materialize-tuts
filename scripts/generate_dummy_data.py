84d7c071ca8b6ac9cadc10ef2037c4c3023f2660
sambeth <sambethslim@gmail.com>
2022-03-21 23:40:21 +0000

    refactor python code to generate dummy data

--- scripts/generate_dummy_data.py
+++ scripts/generate_dummy_data.py
@@ -1,10 +1,7 @@
-import random
-import time
-
 import psycopg2
 
 
-def transact(query):
+def transact(query, action):
     conn = None
 
     try:
@@ -13,12 +10,10 @@ def transact(query):
 
         cur.execute(query)
 
-        product_info = cur.fetchone()
-        print(product_info)
+        product_info = cur.fetchone()[0]
+        print(f"{action} {product_info}")
 
         conn.commit()
-
-        cur.close()
 
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
@@ -26,30 +21,34 @@ def transact(query):
     finally:
         if conn is not None:
             conn.close()
-            print("Closed connection!!")
 
 
 def insert_products(products):
 
     for product_id, amount in products.items():
         sql_insert = f"""
-            INSERT INTO inventory.products(id, amount)
-            VALUE ('{product_id}', '{amount}');
+            INSERT INTO inventory.products(product_id, amount)
+            VALUES ('{product_id}', {amount})
+            RETURNING product_id;
         """
-        transact(sql_insert)
+        transact(sql_insert, "Created")
 
 
 def update_product(product_id, amount):
 
     sql_update = f"""
-           UPDATE inventory.products(id, amount)
+           UPDATE inventory.products
            SET amount = {amount}
-           WHERE id = '{product_id}';
+           WHERE product_id = '{product_id}'
+           RETURNING product_id;
        """
-    transact(sql_update)
+    transact(sql_update, "Updated")
 
 
 if __name__ == "__main__":
+    import random
+    import time
+
     products_dict = {
         'pen': 10,
         'pencil': 5,
